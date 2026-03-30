be088c32dbec3581b44537d3cf218886b6ba5fed
provenzanosimon <provesj@millsaps.edu>
2026-03-29 22:59:39 -0500

    made some fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log("script.js loaded");
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=vLSqUN5pBTeITSKK0AgohJgEofbxQTXW&q=${searchTerm}&limit=12`;
+
 async function fetchGifs(searchTerm) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=vLSqUN5pBTeITSKK0AgohJgEofbxQTXW&q=${searchTerm}&limit=10`;
     const response = await fetch(endpoint);
     const data = await response.json();
 
@@ -12,14 +13,16 @@ async function fetchGifs(searchTerm) {
 
     return images;
 }
+
 const gifContainer = document.querySelector("#gif-container");
 const button = document.querySelector("#fetch-gif-btn");
 const input = document.querySelector("#search-input");
+
 button.addEventListener("click", async () => {
     gifContainer.innerHTML = ""; 
 
-    const images = await fetchGifs(searchTer,);
     const searchTerm = input.value;
+    const images = await fetchGifs(searchTerm); 
 
     images.forEach(url => {
         gifContainer.innerHTML += `<img src="${url}" class="col-3 mb-3">`;
