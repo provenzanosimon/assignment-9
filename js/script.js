086a3128566a98d1179b3ee556a9620af7bdb083
provenzanosimon <provesj@millsaps.edu>
2026-03-29 22:08:22 -0500

    Fetched data from Giphy API and extracted image URLs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("script.js loaded");
+const endpoint = "https://api.giphy.com/v2/emoji?api_key=vLSqUN5pBTeITSKK0AgohJgEofbxQTXW&limit=10&offset=0"
+async function fetchGifs() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    console.log(data);
+
+    const images = data.data.map(gif => gif.images.original.url);
+
+    console.log(images);
+
+    return images;
+}
